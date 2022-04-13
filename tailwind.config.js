2aade926c6f5251f8471365a9a9e6daa13dfaa16
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2022-04-13 23:36:12 +0100

    worked on the font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        body:["Nunito"]
+      }
+    },
   },
   plugins: [],
 };
